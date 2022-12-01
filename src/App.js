d103ec93d1edba8a0b53dcce5086f19cc32b81c7
Gaetan <gaetankas@hotmail.com>
2022-12-01 11:51:58 +0100

    Finished 1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./contain
 import { CTA, Brand, Navbar } from "./components";
 import './App.css'
 
+
 const App = () => {
 
   return (
+    
 
     <div className='App'>
         <div className='gradient__bg'>
